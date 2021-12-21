223b8e1ed459447afb151c646208517ac2b17fe0
retr00h <fabio.cire.156@gmail.com>
2021-12-21 17:14:52 +0100

    Part One of 2015/7_Some_Assembly_Required

--- 2015/7_Some_Assembly_Required.py
+++ 2015/7_Some_Assembly_Required.py
@@ -69,85 +69,79 @@ def partOne(instructions: list[str]) -> int:
     if (instruction.startswith("NOT")): instruction = instruction[4:]
     operands = instruction.split(" -> ")
     return operands[0], operands[1]
-  
-  # If the operands are values, leaves them as is, else (they are registers), access them;
-  # if they are in memory, leave them as is, else (they are not in memory) use the default value.
-  def accessRegisters(firstOperand: str, secondOperand: str):
-    if (firstOperand.isdigit()):
-          if (not secondOperand.isdigit()):
-            secondOperand = memory.get(secondOperand)
-            if (secondOperand == None): secondOperand = defVal
-    else:
-      if (secondOperand.isdigit()):
-        firstOperand = memory.get(firstOperand)
-        if (firstOperand == None): firstOperand = defVal
-      else:
-        firstOperand = memory.get(firstOperand)
-        secondOperand = memory.get(secondOperand)
-        if (firstOperand == None): firstOperand = defVal
-        if (secondOperand == None): secondOperand = defVal
-  
-    return firstOperand, secondOperand
 
   # Dictionary object used as memory
   memory = dict[str, int]()
-
-  # Default value used for empty registers
-  defVal = 0
   
   # Compute each instruction
-  for instruction in instructions:
-    if (instruction.startswith("NOT")):
-      # If the instruction is a NOT instruction, find the operands, then if the left operand
-      # is a digit, perform the NOT operation and save the result in the target register,
-      # else (the left operand is a register) access the register; if they are in memory,
-      # leave them as is, else (they are not in memory) use the default value.
-      # Perform the NOT operation and save the result in the target register.
-      leftOperand, targetRegister = findOperands(instruction)
-      if (leftOperand.isdigit()): memory.update({targetRegister : 65535 - int(leftOperand)})
-      else:
-        operand = memory.get(leftOperand)
-        if (operand == None): operand = defVal
-        memory.update({targetRegister : 65535 - int(operand)})
-    else:
-      # If the instraction is not a NOT instruction, it is either an assignment or a binary
-      # operation. Either way, find the operands
-      leftOperand, targetRegister = findOperands(instruction)
-      if ("AND" in leftOperand):
-        # The instruction is an AND instruction, find the operands, perform the AND operation
-        # and save the result in the target register.
-        operands = leftOperand.split(" AND ")
-        firstOperand, secondOperand = operands[0], operands[1]
-        firstOperand, secondOperand = accessRegisters(firstOperand, secondOperand)
-        memory.update({targetRegister : int(firstOperand) & int(secondOperand)})
-      elif ("OR" in leftOperand):
-        # The instruction is an OR instruction, find the operands, perform the OR operation
-        # and save the result in the target register.
-        operands = leftOperand.split(" OR ")
-        firstOperand, secondOperand = operands[0], operands[1]
-        firstOperand, secondOperand = accessRegisters(firstOperand, secondOperand)
-        memory.update({targetRegister : int(firstOperand) | int(secondOperand)})
-      elif ("LSHIFT" in leftOperand):
-        # The instruction is a LEFT SHIFT instruction, find the operands, perform the LEFT SHIFT
-        # operation and save the result in the target register
-        operands = leftOperand.split(" LSHIFT ")
-        firstOperand, secondOperand = operands[0], operands[1]
-        firstOperand, secondOperand = accessRegisters(firstOperand, secondOperand)
-        memory.update({targetRegister : int(firstOperand) << int(secondOperand)})
-      elif ("RSHIFT" in leftOperand):
-        # The instruction is a RIGHT SHIFT instruction, find the operands, perform the RIGHT SHIFT
-        # operation and save the result in the target register
-        operands = leftOperand.split(" RSHIFT ")
-        firstOperand, secondOperand = operands[0], operands[1]
-        firstOperand, secondOperand = accessRegisters(firstOperand, secondOperand)
-        memory.update({targetRegister : int(firstOperand) >> int(secondOperand)})
-      else:
-        # The instruction is an assignment, either from a register or a direct assignment
-        if (leftOperand.isdigit()): memory.update({targetRegister : int(leftOperand)})
+  while (len(instructions) != 0):
+    newInstructions = list[str]()
+    for instruction in instructions:
+      if (instruction.startswith("NOT")):
+        operands = instruction[4:].split(" -> ")
+        leftOperand = operands[0]
+        targetRegister = operands[1]
+        if (leftOperand.isdigit()): memory.update({targetRegister : 65535 - int(leftOperand)})
         else:
-          leftOperand = memory.get(leftOperand)
-          if (leftOperand == None): leftOperand = defVal
-          memory.update({targetRegister : leftOperand})
+          if (leftOperand in memory): memory.update({targetRegister : 65535 - int(memory.get(leftOperand))})
+          else: newInstructions.append(instruction)
+      else:
+        operands = instruction.split(" -> ")
+        targetRegister = operands[1]
+        operands = operands[0]
+        if ("AND" in operands):
+          operands = operands.split(" AND ")
+          op1, op2 = operands[0], operands[1]
+          if (op1.isdigit()):
+            if (op2.isdigit()): memory.update({targetRegister : int(op1) & int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(op1) & memory.get(op2)})
+            else: newInstructions.append(instruction)
+          elif (op1 in memory):
+            if (op2.isdigit()): memory.update({targetRegister : int(memory.get(op1)) & int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(memory.get(op1)) & int(memory.get(op2))})
+            else: newInstructions.append(instruction)
+          else: newInstructions.append(instruction)
+        elif ("OR" in operands):
+          operands = operands.split(" OR ")
+          op1, op2 = operands[0], operands[1]
+          if (op1.isdigit()):
+            if (op2.isdigit()): memory.update({targetRegister : int(op1) | int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(op1) | memory.get(op2)})
+            else: newInstructions.append(instruction)
+          elif (op1 in memory):
+            if (op2.isdigit()): memory.update({targetRegister : int(memory.get(op1)) | int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(memory.get(op1)) | int(memory.get(op2))})
+            else: newInstructions.append(instruction)
+          else: newInstructions.append(instruction)
+        elif ("LSHIFT" in operands):
+          operands = operands.split(" LSHIFT ")
+          op1, op2 = operands[0], operands[1]
+          if (op1.isdigit()):
+            if (op2.isdigit()): memory.update({targetRegister : int(op1) << int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(op1) << memory.get(op2)})
+            else: newInstructions.append(instruction)
+          elif (op1 in memory):
+            if (op2.isdigit()): memory.update({targetRegister : int(memory.get(op1)) << int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(memory.get(op1)) << int(memory.get(op2))})
+            else: newInstructions.append(instruction)
+          else: newInstructions.append(instruction)
+        elif ("RSHIFT" in operands):
+          operands = operands.split(" RSHIFT ")
+          op1, op2 = operands[0], operands[1]
+          if (op1.isdigit()):
+            if (op2.isdigit()): memory.update({targetRegister : int(op1) >> int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(op1) >> memory.get(op2)})
+            else: newInstructions.append(instruction)
+          elif (op1 in memory):
+            if (op2.isdigit()): memory.update({targetRegister : int(memory.get(op1)) >> int(op2)})
+            elif (op2 in memory): memory.update({targetRegister : int(memory.get(op1)) >> int(memory.get(op2))})
+            else: newInstructions.append(instruction)
+          else: newInstructions.append(instruction)
+        else:
+          if (operands.isdigit()): memory.update({targetRegister : int(operands)})
+          elif (operands in memory): memory.update({targetRegister : int(memory.get(operands))})
+          else: newInstructions.append(instruction)
+    instructions = newInstructions
   
   return memory
 
