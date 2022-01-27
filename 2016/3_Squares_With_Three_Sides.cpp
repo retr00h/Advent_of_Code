97a1cc107b0a88d817fb99ff2688cb22bbb16f5b
retr00h <fabio.cire.156@gmail.com>
2022-01-27 09:31:46 +0100

    Part Two of 2016/3_Squares_With_Three_Sides

--- 2016/3_Squares_With_Three_Sides.cpp
+++ 2016/3_Squares_With_Three_Sides.cpp
@@ -17,6 +17,23 @@
  * because 5 + 10 is not larger than 25.
  *
  * In your puzzle input, how many of the listed triangles are possible?
+ *
+ *
+ * --- Part Two ---
+ *
+ * Now that you've helpfully marked up their design documents, it occurs to
+ * you that triangles are specified in groups of three vertically. Each set of
+ * three numbers in a column specifies a triangle. Rows are unrelated.
+ * For example, given  the following specification, numbers with the same
+ * hundreds digit would be part of the same triangle:
+ *   101 301 501
+ *   102 302 502
+ *   103 303 503
+ *   201 401 601
+ *   202 402 602
+ *   203 403 603
+ * In your puzzle input, and instead reading by columns, how many of the
+ * listed triangles are possible?
  */
 
 #include <iostream>
@@ -25,13 +42,11 @@
 
 using namespace std;
 
-//#define debug true
 
 vector<vector<int>> readInput() {
   vector<vector<int>> numbers{};
   ifstream f("../2016/input/3_Squares_With_Three_Sides.txt");
   int a, b, c;
-  bool first = true;
   while (f >> a >> b >> c) {
     vector<int> arr = {a, b, c};
     numbers.emplace_back(arr);
@@ -46,27 +61,28 @@ int partOne(vector<vector<int>> numbers) {
     int a = numbers[i][0];
     int b = numbers[i][1];
     int c = numbers[i][2];
-    #ifdef debug
-      cout << a << " " << b << " " << c << endl;
-    #endif
 
     if (a + b > c and a + c > b and b + c > a) ++counter;
   }
   return counter;
 }
 
+int partTwo(vector<vector<int>> numbers) {
+  int counter = 0;
+  for (int i = 0; i < numbers.size() - 2; i+= 3) {
+    for (int j = 0; j < 3; j++) {
+      int a = numbers[i][j];
+      int b = numbers[i+1][j];
+      int c = numbers[i+2][j];
+      if (a + b > c and a + c > b and b + c > a) ++counter;
+    }
+  }
+  return counter;
+}
+
 int main() {
   vector<vector<int>> numbers = readInput();
-  cout << numbers[0][0] << " " << numbers[0][1] << " " << numbers[0][2] << endl;
-  #ifdef debug
-    vector<int*> test{};
-    int arr[3] = {5,10,25};
-    test.emplace_back(arr);
-    cout << "Test: " << partOne(test) << endl;
-  #endif
-
-  #ifndef debug
-    cout << "Part One: " << partOne(numbers) << endl;
-  #endif
+  cout << "Part One: " << partOne(numbers) << endl; // 1050
+  cout << "Part Two: " << partTwo(numbers) << endl; // 1921
   return 0;
 }
