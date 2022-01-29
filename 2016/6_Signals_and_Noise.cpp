957fb40bad200b1fc103c5b1ef36ab858aa6334a
retr00h <fabio.cire.156@gmail.com>
2022-01-29 16:35:22 +0100

    Part Two of 2016/6_Signals_and_Noise

--- 2016/6_Signals_and_Noise.cpp
+++ 2016/6_Signals_and_Noise.cpp
@@ -34,6 +34,24 @@
  *
  * Given the recording in your puzzle input, what is the error-corrected
  * verison of the message being sent?
+ *
+ *
+ * --- Part Two ---
+ *
+ * Of course, that would be the message - if you hadn't agreed to use a
+ * modified repetition code instead.
+ *
+ * In this modified code, the sender instead transmits what looks like random
+ * data, but for each character, the character they actually want to send is
+ * slightly less likely than the others. Even after signal-jamming noise, you
+ * can look at the letter distrubutions in each column and choose the least
+ * common letter to reconstruct the original message.
+ * In the above example, the least common character in the first column is a;
+ * in the second, d, and so on. Repeating this process for the remaining
+ * characters produces the original message, advent.
+ *
+ * Given the recording in your puzzle input and this new decoding methodology,
+ * what is the original message that Santa is trying to send?
  */
 
 #include <iostream>
@@ -71,9 +89,31 @@ string partOne(const vector<string> &signals) {
   return correctedSignal;
 }
 
+string partTwo(const vector<string> &signals) {
+  string correctedSignal = "";
+  for (int i = 0; i < signals[0].length(); i++) {
+    int occurrences[26] = {0};
+    for (const string& sig : signals) {
+      int x = sig[i] - 'a';
+      occurrences[x]++;
+    }
+    char c;
+    int leastCommon = signals.size() + 1;
+    for (int j = 0 ; j < 26; j++) {
+      if (occurrences[j] != 0 and occurrences[j] < leastCommon) {
+        leastCommon = occurrences[j];
+        c = 'a' + j;
+      }
+    }
+    correctedSignal += c;
+  }
+  return correctedSignal;
+}
+
 int main() {
   vector<string> signals{};
   readInput(signals);
   cout << "Part One: " << partOne(signals) << endl; // afwlyyyq
+  cout << "Part Two: " << partTwo(signals) << endl; // bhkzekao
   return 0;
 }
