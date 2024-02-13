3ced357ade0db9383cea7259e238a320a1f7b9fd
Fabio <fabio.cire.156@gmail.com>
2024-02-13 09:38:42 +0100

    Solved Part Two of 2023/1_Trebuchet in C++

--- 2023/1_Trebuchet/main.cpp
+++ 2023/1_Trebuchet/main.cpp
@@ -66,15 +66,13 @@ What is the sum of all of the calibration values?
 */
 
 #undef TEST
-#define TEST
+// #define TEST
 
 #include <iostream>
 #include <fstream>
 
-#ifdef TEST
 #include <vector>
 #include <sstream>
-#endif
 
 int partOne() {
     int sum = 0;
@@ -105,24 +103,21 @@ int partTwo() {
     int sum = 0;
     std::string line;
     std::ifstream f;
+    size_t pos;
     f.open("input/1_Trebuchet.txt");
     while (!f.eof()) {
         int first = -1, last = -1;
         std::string tmp;
-        std::size_t found;
         f >> line;
 
-        for (int i = 0; i < line.length(); i++) {
-            for (int j = 0; j < 3; j++) {
-               tmp = line.substr(i, 3 + j);
-               for (int k = 0; k < 9; k++) {
-                if (tmp == conv[k]) {
-                    line.replace(i, 3 + j, std::to_string(k + 1));
-                    break;
-                }
-               }
+        for (int i = 0; i < 9; i++) {
+            pos = line.find(conv[i]);
+            while (pos != std::string::npos) {
+                line.replace(pos, conv[i].length(), conv[i] + std::to_string(i + 1) + conv[i]);
+                pos = line.find(conv[i], pos + (2 * conv[i].length()) + 1);
             }
         }
+        auto x = 1;
 
         for (int i = 0; i < line.length(); i++) {
             char c1 = line[i] - '0', c2 = line[line.length() - 1 - i] - '0';
@@ -135,81 +130,13 @@ int partTwo() {
             if (first != -1 && last != -1) break;
         }
         sum += (first + last);
-
     }
     f.close();
     return sum;
 }
 
-void partTwoTest() {
-    static std::string conv[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-    int passed = 0, failed = 0;
-    std::vector<int> failedIdx = std::vector<int>();
-    std::string line;
-    std::ifstream f;
-    f.open("input/1_Trebuchet.txt");
-
-    int idx = 0;
-    int elements[50] = {
-        21, 69, 39, 74, 45, 86, 84, 62, 27, 56,
-        32, 51, 75, 73, 48, 18, 23, 35, 17, 66,
-        33, 15, 18, 14, 95, 82, 82, 59, 34, 79,
-        41, 84, 46, 76, 65, 78, 67, 72, 67, 62,
-        68, 45, 25, 14, 66, 55, 93, 44, 13, 29
-    };
-    while (!f.eof()) {
-        int first = -1, last = -1;
-        std::string tmp;
-        std::size_t found;
-        f >> line;
-
-        for (int i = 0; i < line.length(); i++) {
-            for (int j = 0; j < 3; j++) {
-               tmp = line.substr(i, 3 + j);
-               for (int k = 0; k < 9; k++) {
-                if (tmp == conv[k]) {
-                    line.replace(i, 3 + j, std::to_string(k + 1));
-                    break;
-                }
-               }
-            }
-        }
-
-        for (int i = 0; i < line.length(); i++) {
-            char c1 = line[i] - '0', c2 = line[line.length() - 1 - i] - '0';
-            if (first == -1 && c1 >= 0 && c1 <= 9) {
-                first = 10 * c1;
-            }
-            if (last == -1 && c2 >= 0 && c2 <= 9) {
-                last = c2;
-            }
-            if (first != -1 && last != -1) break;
-        }
-        if (idx < 50) {
-            int el = elements[idx++];
-            if (first + last == el) passed++;
-            else {
-                failed++;
-                failedIdx.emplace_back(idx - 1);
-            }
-        }
-    }
-    std::cout << "Passed: " << passed << std::endl;
-    std::cout << "Failed: " << failed << std::endl;
-    if (!failedIdx.empty()) {
-        std::stringstream ss;
-        for (int idx : failedIdx) ss << (std::to_string(idx) + " ");
-        std::cout << "Failed test cases #: " << ss.str() << std::endl;
-    }
-    f.close();
-}
-
 int main() {
     std::cout << "Part one: " << partOne() << std::endl;
-    #ifdef TEST
-    partTwoTest();
-    #else
     std::cout << "Part two: " << partTwo() << std::endl;
-    #endif
     return 0;
 }
